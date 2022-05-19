f018e1d7efe12e280701801867cc17cfab082074
backdevruso <feedback0225@gmail.com>
2022-05-19 17:57:08 +0300

    all sections

---
+++ src/assets/js/partners-slider.js
@@ -0,0 +1,9 @@
+
+if(document.querySelector('.partners__slider')) {
+    let partnersSlider = new Swiper(".partners__slider", {
+        slidesPerView: "auto",
+        spaceBetween: 30,
+        grabCursor: true,
+        loop: true,
+    });
+}
