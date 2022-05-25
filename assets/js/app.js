cd3d11fe559b71cef671ffb356763c442b608750
backdevruso <feedback0225@gmail.com>
2022-05-25 07:50:32 +0300

    fixing menu closing by link

--- assets/js/app.js
+++ assets/js/app.js
@@ -352,3 +352,12 @@ if (smoothLinks) {
         });
     };
 }
+
+if (document.querySelector('.menu')) {
+    document.querySelectorAll('.menu ul a').forEach(function(link) {
+        link.addEventListener('click', function() {
+            document.querySelector('body').classList.remove('m-open');
+            document.querySelector('.burger').classList.remove('isOpen');
+        })
+    });
+}
