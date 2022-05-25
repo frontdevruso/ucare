f8d4efc343a11987cf53838992e2fab753242d00
frontdevruso <frontdevruso@gmail.com>
2022-05-25 13:57:23 +0300

    fixing header

--- assets/js/app.js
+++ assets/js/app.js
@@ -361,3 +361,22 @@ if (document.querySelector('.menu')) {
         })
     });
 }
+
+if (document.querySelector('.header')) {
+    const header = document.querySelector('.header');
+    const headerHeight = parseInt(window.getComputedStyle(header).height);
+
+    const checkingHeaderPosition = () => {    
+        if (window.scrollY > headerHeight / 2) {
+            header.classList.add('header--floating');
+        } else {
+            header.classList.remove('header--floating');
+        }
+    }
+
+    checkingHeaderPosition();
+
+    window.addEventListener("scroll", function() {
+        checkingHeaderPosition();
+    });
+}
